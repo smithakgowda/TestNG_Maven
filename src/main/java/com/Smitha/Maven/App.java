ebbe49042a1f08005452b53e0b66fda26fd8f92b
00003517 <00003517@AGI-LT-1601.arisglobal.com>
2021-09-23 16:40:57 +0530

    commit to TestNG maven

---
+++ src/main/java/com/Smitha/Maven/App.java
@@ -0,0 +1,13 @@
+package com.Smitha.Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
