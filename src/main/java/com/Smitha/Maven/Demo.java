f8d4bd64a15129a10f35149f2492d50ce7dc53c9
00003517 <00003517@AGI-LT-1601.arisglobal.com>
2021-09-23 16:46:02 +0530

    second commit

--- src/main/java/com/Smitha/Maven/Demo.java
+++ src/main/java/com/Smitha/Maven/Demo.java
@@ -1,9 +1,20 @@
 package com.Smitha.Maven;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class Demo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+		 System.setProperty("webdriver.chrome.driver","C:\\SVN\\Automation\\Automation_Course\\chromedriver_win32\\chromedriver.exe");
+		 WebDriver myD=new ChromeDriver();
+		 myD.get("https://www.google.com/");
+		 myD.manage().window().maximize();
+		 String vTitle=myD.getTitle();
+		 System.out.println("Title of the screen is " +vTitle);
+		 Thread.sleep(5000);
+		 myD.close();
 
 	}
 
