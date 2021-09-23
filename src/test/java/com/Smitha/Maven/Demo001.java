ccede8c28d3a2618f1bbf2ad47ebc63fadd5a1b0
00003517 <00003517@AGI-LT-1601.arisglobal.com>
2021-09-23 16:56:13 +0530

    commit

--- src/test/java/com/Smitha/Maven/Demo001.java
+++ src/test/java/com/Smitha/Maven/Demo001.java
@@ -3,11 +3,10 @@ package com.Smitha.Maven;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-public class Demo {
+public class Demo001 {
 
 	public static void main(String[] args) throws InterruptedException {
-		 System.setProperty("webdriver.chrome.driver","C:\\SVN\\Automation\\Automation_Course\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\SVN\\Automation\\Automation_Course\\chromedriver_win32\\chromedriver.exe");
 		 WebDriver myD=new ChromeDriver();
 		 myD.get("https://www.google.com/");
 		 myD.manage().window().maximize();
@@ -15,6 +14,7 @@ public class Demo {
 		 System.out.println("Title of the screen is " +vTitle);
 		 Thread.sleep(5000);
 		 myD.close();
+		 
 
 	}
 
